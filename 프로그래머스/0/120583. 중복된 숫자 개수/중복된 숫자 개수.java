3b20460d3008554bf50d97a9424420127a461cd0
playous <149496018+playous@users.noreply.github.com>
2025-04-09 15:56:49 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.05 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        for (int arr : array){
+            if(arr == n) answer++;
+        }
+        return answer;
+    }
+}
