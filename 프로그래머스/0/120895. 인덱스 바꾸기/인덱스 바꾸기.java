cb006efe790aeaedcc78df2d373af8e2b44df52d
playous <149496018+playous@users.noreply.github.com>
2025-04-09 17:18:28 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] ch = my_string.toCharArray();
+       
+        char temp = ch[num1];
+        ch[num1] = ch[num2];
+        ch[num2] = temp;
+        
+        String answer = new String(ch);
+        
+        return answer;
+    }
+}
