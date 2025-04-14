c7b7c45c4245ddf308efba15f5165f4d2f429e48
playous <149496018+playous@users.noreply.github.com>
2025-04-14 15:59:54 +0900

    [level 0] Title: 소인수분해, Time: 0.53 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -2,31 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        int[] prime = new int[10001];
-        ArrayList<Integer> list = new ArrayList<>();
         Set<Integer> set = new HashSet<>();
         
-        
-        prime[0] = prime[1] = 1; 
-      
-        for (int i = 2; i <= Math.sqrt(10001) ; i++){
-            if(prime[i] == 0){
-                for (int j = i * i ; j <= 10000 ; j += i){
-                    prime[j] = 1;
-                }
-            }
-        }
-       
-        for (int i = 2 ; i < 10001 ; i ++){
-            if(prime[i] == 0) list.add(i);
-        }
-        
-        for(int i : list){
+        for (int i = 2 ; i <= n ; i ++){
             while(n % i == 0){
-                n /= i;
                 set.add(i);
+                n /= i;
             }
         }
+        
         
         int[] answer = new int[set.size()];
         int index = 0;
