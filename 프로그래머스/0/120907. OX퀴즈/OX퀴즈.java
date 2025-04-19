f351d80445cacd711c6c749a20e2bdc2a2d75306
playous <149496018+playous@users.noreply.github.com>
2025-04-19 16:20:10 +0900

    [level 0] Title: OX퀴즈, Time: 0.29 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -16,11 +16,7 @@ class Solution {
                 else num += (a * Integer.parseInt(n));
             }
             String sum_s = s.substring(s.indexOf("=") + 2, s.length());
-            System.out.println(sum_s);
-            System.out.println("----------------");
             int sum = sum_s.contains("-") ? -1 * Integer.parseInt(sum_s.substring(1,sum_s.length())) : Integer.parseInt(sum_s);
-            System.out.println(num);
-            System.out.println(sum);
             
             answer[index++] = (num == sum) ? "O" :"X";
         }
--- 프로그래머스/0/120907. OX퀴즈/README.md
+++ 프로그래머스/0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907#) 
 
 ### 성능 요약
 
-메모리: 98.1 MB, 시간: 1.49 ms
+메모리: 83.2 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 19일 16:19:42
+2025년 04월 19일 16:20:08
 
 ### 문제 설명
 
