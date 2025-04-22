9c81ff129b38b2eb1637cf8e62bcc3b09af91756
playous <149496018+playous@users.noreply.github.com>
2025-04-22 14:07:06 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.23 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        // n이 홀수인 경우 불가능
+        long[] arr = new long[n+1];
+        arr[2] = 3;
+        arr[4] = 11;
+        for (int i = 6 ; i <= n ;i +=2){
+            arr[i] = ((arr[i-2] * 4) % 1000000007 - arr[i-4] + 1000000007) % 1000000007; 
+        }
+        return (int)arr[n];
+    }
+}
