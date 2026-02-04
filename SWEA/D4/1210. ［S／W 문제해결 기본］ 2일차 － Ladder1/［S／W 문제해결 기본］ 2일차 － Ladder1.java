c0a8a6fafdace12d582a36c0e69d739274142118
park minkyu <149496018+playous@users.noreply.github.com>
2026-02-04 15:47:00 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 135 ms, Memory: 36,860 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -2,43 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+    
+    static int[] dr = {0, 0, 1};
+    static int[] dc = {-1, 1, 0};
+    
+    static boolean[][] visited;
+    static int[][] map;
+    static int endCol;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         for (int t = 0; t < 10; t++) {
             int n = Integer.parseInt(br.readLine());
-            int end = 0;
-            int[][] map = new int[100][100];
+            int answer = 0;
+            map = new int[100][100];
+            
             for (int i = 0; i < 100; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < 100; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
-                    if (i == 99) {
-                        if (map[i][j] == 2) end = j;
+                    if (i == 99 && map[i][j] == 2) endCol = j;
+                }
+            }
+            
+            for (int j = 0; j < 100 ; j ++){
+                if(map[0][j] == 1){
+                    visited = new boolean[100][100];
+                    if (dfs(0, j) == endCol) {
+                        answer = j;
+                        break;
                     }
                 }
             }
-            for (int i = 99 ; i > 0 ; i --){
-                if (end - 1 >= 0 && map[i][end-1] == 1){
-                    while(map[i][end-1] == 1){
-                        end--;
-                        if (end == 0) break;
-                    }
-                }
-                else if (end + 1 <= 99 && map[i][end+1] == 1){
-                    while(map[i][end+1] == 1){
-                        end++;
-                        if (end == 99) break;
-                    }
-                }
-            }
-            sb.append("#").append(n).append(" ").append(end).append("\n");
+            sb.append("#").append(n).append(" ").append(answer).append("\n");
         }
-
-        bw.write(sb.toString());
-        bw.flush();
+        System.out.print(sb.toString());
+    }
+    
+    public static int dfs(int r, int c){
+        visited[r][c] = true;
+        if(r == 99){
+            return c;
+        }
+        
+        if(c - 1 >= 0 && map[r][c-1] == 1 && !visited[r][c-1] ) return dfs(r, c-1);
+		else if(c + 1 < 100 && map[r][c+1] == 1 && !visited[r][c+1]) return dfs(r, c+1);
+		else return dfs(r+1, c); 
     }
 }
