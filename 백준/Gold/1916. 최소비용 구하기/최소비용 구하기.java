8b4dd8efcfa83ed9c1c908f79bd3f9e5439b573b
park minkyu <149496018+playous@users.noreply.github.com>
2026-02-09 20:44:21 +0900

    [Gold V] Title: 최소비용 구하기, Time: 464 ms, Memory: 51748 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,62 +2,68 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class City {
+
+    static class Node{
         int name;
         int cost;
-        public City(int name, int cost){
+        Node(int name, int cost){
             this.name = name;
             this.cost = cost;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        List<Node>[] list = new List[n + 1];
 
-        List<City>[] list = new List[n + 1];
         for (int i = 1; i <= n; i++) {
             list[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < m; i++) {
+        int m = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < m ; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            list[a].add(new City(b, c));
+            list[a].add(new Node(b, c));
         }
 
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        int[] cost = new int[n + 1];
+        int[] dist = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
 
         for (int i = 1; i <= n; i++) {
-            if (i != start){
-                cost[i] = Integer.MAX_VALUE;
-            }
+            dist[i] = Integer.MAX_VALUE;
         }
+        dist[start] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
+        pq.add(new Node(start, 0));
 
-        PriorityQueue<City> q = new PriorityQueue<>((a,b) -> a.cost - b.cost);
-        q.add(new City(start, 0));
-        cost[start] = 0;
-        while (!q.isEmpty()){
-            City cur = q.poll();
-            if (cur.cost > cost[cur.name]) continue;
-            for (City c : list[cur.name]) {
-                if (cost[c.name] > cost[cur.name] + c.cost){
-                    cost[c.name] = cost[cur.name] + c.cost;
-                    q.add(new City(c.name, cost[c.name]));
+        while (!pq.isEmpty()){
+            Node cur = pq.poll();
+
+            int name = cur.name;
+            int cost = cur.cost;
+            visited[name] = true;
+
+            if(cost > dist[name]) continue;
+            
+            for (Node next : list[name]){
+                if(visited[next.name]) continue;
+                if (cost + next.cost < dist[next.name]){
+                    dist[next.name] = cost + next.cost;
+                    pq.add(new Node(next.name, dist[next.name]));
                 }
             }
         }
 
-        bw.write(String.valueOf(cost[end]));
-        bw.flush();
+        System.out.println(dist[end]);
     }
 }
