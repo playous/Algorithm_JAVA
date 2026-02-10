6c6f54d07a5de2860cb5f6dfc725b85ff1fa9b08
park minkyu <149496018+playous@users.noreply.github.com>
2026-02-10 14:28:27 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 710 ms, Memory: 122,440 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,113 +1,146 @@
 import java.util.*;
 import java.io.*;
 
-public class Solution {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	static int T;
-
-	static int N;
-	static int[][] board;
-	static List<int[]> processors;
-	static int[][] dList = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-
-	static int maxCore;
-	static int maxLine;
-
-	static void dfs(int n, int lineCount, int coreCount) {
-		if(coreCount + processors.size() - n < maxCore)
-			return;
-		if (n == processors.size()) {
-			if (coreCount > maxCore) {
-				maxLine = lineCount;
-				maxCore = coreCount;
-			} else if (coreCount == maxCore) {
-				maxLine = Math.min(lineCount, maxLine);
-			}
-			return;
+class Solution{
+	static int n;
+	static int[][] map;
+	
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	static int[] answer;
+	
+	static List<Node> cores;
+	static int coreCnt;
+	
+	static class Node{
+		int r;
+		int c;
+		Node(int r, int c){
+			this.r = r;
+			this.c = c;
 		}
-
-		// 0 1 2 3
-		int[] cur = processors.get(n);
-		int x = cur[0];
-		int y = cur[1];
-
-		for (int[] d : dList) {
-			boolean canConnect = false;
-			for (int i = 1; i < N + 1; i++) {
-				int nx = x + d[0] * i;
-				int ny = y + d[1] * i;
-
-				if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
-					canConnect = true;
-					break;
-				}
-				if (board[nx][ny] != 0)
-					break;
-			}
-			if (canConnect) {
-				int lCount = 0;
-				for (int i = 1; i < N + 1; i++) {
-					int nx = x + d[0] * i;
-					int ny = y + d[1] * i;
-
-					if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-						break;
-
-					board[nx][ny] = 2;
-					lCount++;
-				}
-
-				dfs(n + 1, lineCount + lCount, coreCount + 1);
-				for (int i = 1; i < N + 1; i++) {
-					int nx = x + d[0] * i;
-					int ny = y + d[1] * i;
-
-					if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-						break;
-
-					if (board[nx][ny] == 2)
-						board[nx][ny] = 0;
-				}
-			}
-		}
-		dfs(n + 1, lineCount, coreCount);
-
 	}
-
-	public static void main(String[] args) throws Exception {
-		T = Integer.parseInt(br.readLine());
-
-		for (int t = 1; t < T + 1; t++) {
-			sb.append("#").append(t).append(" ");
-			N = Integer.parseInt(br.readLine());
-			board = new int[N][N];
-			processors = new ArrayList<>();
-			maxLine = 0;
-			maxCore = 0;
-
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					int num = Integer.parseInt(st.nextToken());
-					board[i][j] = num;
-					if (num == 1) {
-						if (i != 0 && i != N - 1 && j != 0 && j != N - 1)
-							processors.add(new int[] { i, j });
-					}
-				}
-			}
-
-			dfs(0, 0, 0);
-			sb.append(maxLine);
-			sb.append("\n");
+	
+	public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int t = 1; t <= T; t++) {
+        	
+        	n = Integer.parseInt(br.readLine());
+        	
+        	map = new int[n][n];
+        	boolean[][] visited = new boolean[n][n];
+        	cores = new ArrayList<>();
+        	
+        	
+        	for (int i = 0; i < n ; i ++) {
+        		st = new StringTokenizer(br.readLine());
+        		for (int j = 0; j < n ; j ++) {
+        			map[i][j] = Integer.parseInt(st.nextToken());
+        			if (map[i][j] == 1) {
+        				if(i != 0 && j != 0 && i != n -1 && j != n - 1) {
+        					cores.add(new Node(i,j));
+        				}
+        				visited[i][j] = true;
+        			}
+        		}
+        	}
+        	coreCnt = cores.size();
+        	
+        	answer = new int[coreCnt + 1];
+        	Arrays.fill(answer, Integer.MAX_VALUE);
+        	
+        	dfs(0, 0, 0, visited);
+        	
+        	int answerCount = 0;
+        	
+        	for (int i = 1 ; i <= coreCnt ; i++) {
+        		if(answer[i] == Integer.MAX_VALUE) break;
+        		answerCount = answer[i];
+        	}
+        	
+        	sb.append("#").append(t).append(" ").append(answerCount).append("\n");
 		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+        
+        System.out.println(sb.toString());
+        
+	}
+	
+	public static void dfs(int idx, int cnt, int sum, boolean[][] visited) {	
+		answer[cnt] = Math.min(answer[cnt], sum);
+		
+		if (idx == coreCnt) return;
+		
+		Node cur = cores.get(idx);
+		int curR = cur.r;
+		int curC = cur.c;
+		
+		boolean[][] save = new boolean[n][n];
+		
+		for (int i = 0 ; i < n ; i ++) {
+			save[i] = visited[i].clone();
+		}
+		
+		
+		for (int i = 0; i < 5 ; i ++) {
+			if (i == 4) {
+				dfs(idx + 1, cnt, sum, visited);
+				continue;
+			}
+			
+			int len = calc(curR, curC, i, visited);
+			
+			if (len == -1) continue;
+			
+			dfs(idx+1, cnt + 1, sum + len, visited);
+			
+			for (int j = 0; j < n; j++) {
+			    visited[j] = save[j].clone();
+			}
+		}
+		
+	}
+	
+	public static int calc(int r, int c, int dirc, boolean[][] visited) {
+		int nextR = r;
+		int nextC = c;
+		int count = 0;
+		
+		while(true) {
+			nextR += dr[dirc];
+			nextC += dc[dirc];
+			
+			if (nextR == 0 || nextC == 0 || nextR == n - 1 || nextC == n - 1) {
+				if (visited[nextR][nextC]) return -1;
+				break;
+			}
+			
+			if (visited[nextR][nextC]) {
+				return -1;
+			}
+		}
+		
+		int minR = Math.min(nextR, r);
+		int maxR = Math.max(nextR, r);
+		
+		int minC = Math.min(nextC, c);
+		int maxC = Math.max(nextC, c);
+		
+		for (int i = minR; i <= maxR ; i++) {
+			visited[i][c] = true;
+			count++;
+		}
+		for (int j = minC; j <= maxC; j++) {
+			visited[r][j] = true;
+			count++;
+		}
+		
+		return count - 2;
 	}
 
 }
