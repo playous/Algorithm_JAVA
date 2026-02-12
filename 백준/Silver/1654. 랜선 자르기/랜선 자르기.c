ca1b9be8f9fee69fdd5d81c893f932b914ab62ca
park minkyu <149496018+playous@users.noreply.github.com>
2026-02-12 13:03:59 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int K, N;
+    scanf("%d %d", &K, &N);
+    int* line = calloc(K, sizeof(int));
+    int cnt, last = 0;
+    for (int i = 0; i < K; i++) {
+        scanf("%d", &line[i]);
+        if (line[i] > last)last = line[i];
+    }
+    long long first = 1, mid, max = 0;
+
+    while (first <= last) {
+        mid = (first + last) / 2, cnt = 0;
+
+        for (int i = 0; i < K; i++)
+            cnt += line[i] / mid;
+
+        if (cnt >= N) {
+            if (max < mid)
+                max = mid;
+            first = mid + 1;
+        }
+        else last = mid - 1;
+    }
+    printf("%lld\n", max);
+}
