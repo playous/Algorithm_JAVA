2049c64d0e3f78d4dca29c83dd1b2d0b084e2c5e
park minkyu <149496018+playous@users.noreply.github.com>
2026-03-09 11:41:53 +0900

    [D4] Title: 서로소 집합, Time: 428 ms, Memory: 105,516 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+	static int n, m;
+	static int[] parents;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T ; t ++) {
+			st = new StringTokenizer(br.readLine());
+			
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			
+			parents = new int[n + 1];
+			
+			for (int i = 1; i <= n ; i ++) {
+				parents[i] = i;
+			}
+			
+			
+			sb.append("#").append(t).append(" ");
+			
+			for (int i = 0 ; i < m ; i ++) {
+				st = new StringTokenizer(br.readLine());
+				
+				int order = Integer.parseInt(st.nextToken());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				
+				if (order == 0) {
+					union(a, b);
+				}
+				if (order == 1) {
+					if(findSet(a) == findSet(b)) sb.append(1);
+					else sb.append(0);
+				}
+				
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.print(sb.toString());
+
+	}
+	
+	public static boolean union(int a, int b) {
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		
+		if (aRoot == bRoot) return false;
+		
+		parents[aRoot] = bRoot;
+		return true;
+	}
+	
+	public static int findSet(int a) {
+		if(a == parents[a]) return a;
+		return parents[a] = findSet(parents[a]);
+	}
+
+}
