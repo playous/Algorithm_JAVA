33fcc6840d1f51abba0d40461d39ad734cf8ca0a
park minkyu <149496018+playous@users.noreply.github.com>
2026-03-10 15:43:39 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,194 ms, Memory: 126,724 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,46 +1,52 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
 
-    static int n;
-    static long[][] dis;
-    static boolean[] visited;
-    static Node[] nodes;
-
-    static class Node{
+	static int[] parents;
+	static int n;
+	static double answer;
+	
+	static double e;
+	
+	static long[][] dis;
+	static Node[] nodes;
+	
+	static class Node{
         int x;
         int y;
     }
-
-    static class Node2{
-        int next;
-        long value;
-        Node2(int next, long value){
-            this.next = next;
-            this.value = value;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int T = Integer.parseInt(br.readLine());
-
-        for (int t = 1 ; t <= T; t++) {
-            n = Integer.parseInt(br.readLine());
-            dis = new long[n + 1][n + 1];
-            visited = new boolean[n + 1];
-            nodes = new Node[n + 1];
-            double answer = 0;
-
-            for (int i = 1; i <= n ; i ++){
-                nodes[i] = new Node();
-            }
-
-            st = new StringTokenizer(br.readLine());
+	
+	static class Node2 {
+		int a;
+		int b;
+		long c;
+		Node2(int a, int b, long c){
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T ; t++) {
+			n = Integer.parseInt(br.readLine());
+			nodes = new Node[n + 1];
+			parents = new int[n + 1];
+			dis = new long[n + 1][n + 1];
+			
+			for (int i = 1; i <= n; i ++) {
+				parents[i] = i;
+				nodes[i] = new Node();
+			}
+			
+			st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= n ; i ++){
                 nodes[i].x = Integer.parseInt(st.nextToken());
             }
@@ -48,53 +54,65 @@ public class Solution {
             for (int i = 1; i <= n ; i ++){
                 nodes[i].y = Integer.parseInt(st.nextToken());
             }
-
+            
             double e = Double.parseDouble(br.readLine());
-
+            
             calDis(1);
+			
+			PriorityQueue<Node2> pq = new PriorityQueue<>((a, b) -> Long.compare(a.c, b.c));
+			
+			for (int i = 1; i <= n ; i++) {
+				for (int j = i + 1; j <= n ; j++) {
+					pq.add(new Node2(i, j, dis[i][j]));
+				}
+			}
+			
+			answer = 0;
+			
+			while(!pq.isEmpty()) {
+				Node2 cur = pq.poll();
+				
+				union(cur.a, cur.b, cur.c * e);
+			}
+			
+			sb.append("#").append(t).append(" ").append(Math.round(answer)).append("\n");
+			
+		}
+		System.out.print(sb.toString());
+		
 
-            PriorityQueue<Node2> pq = new PriorityQueue<>((a, b) -> Long.compare(a.value, b.value));
-
-            for (int i = 2; i <= n; i++) {
-                pq.add(new Node2(i, dis[1][i]));
-            }
-            visited[1] = true;
-
-            while(!pq.isEmpty()) {
-                Node2 cur = pq.poll();
-                int next = cur.next;
-                double value = cur.value;
-                if (visited[next]) continue;
-                visited[next] = true;
-                answer += (value * e);
-
-                for (int i = 2; i <= n; i++) {
-                    if (!visited[i]) pq.add(new Node2(i, dis[next][i]));
-                }
-            }
-
-            sb.append("#").append(t).append(" ").append(Math.round(answer)).append("\n");
-        }
-
-
-        System.out.println(sb.toString());
-    }
-
-    static void calDis(int idx){
+	}
+	
+	public static void union(int a, int b, double c) {
+		int rootA = findSet(a);
+		int rootB = findSet(b);
+		
+		if(rootA == rootB) return;
+		
+		parents[rootA] = rootB;
+		answer += c;
+	}
+	
+	public static int findSet(int a) {
+		if (a == parents[a]) return a;
+		return parents[a] = findSet(parents[a]);
+	}
+	
+	static void calDis(int idx){
         if (idx == n) return;
-
+ 
         int curX = nodes[idx].x;
         int curY = nodes[idx].y;
-
+ 
         for (int i = idx + 1; i <= n ; i ++){
             int nextX = nodes[i].x;
             int nextY = nodes[i].y;
             double dis1 = Math.pow((curX - nextX), 2) + Math.pow((curY - nextY), 2);
-
+ 
             dis[idx][i] = (long)dis1;
             dis[i][idx] = (long)dis1;
         }
-
+ 
         calDis(idx + 1);
     }
 }
