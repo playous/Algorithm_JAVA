9fb178262136e7ace7ca26d7d985fd1dd66e92e9
park minkyu <149496018+playous@users.noreply.github.com>
2026-04-02 21:24:11 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.85 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,24 +1,55 @@
 import java.util.*;
 
 class Solution {
+    
+    int n;
+    int[] cards;
+    boolean[] visited;
+    List<Integer> list;
+
     public int solution(int[] cards) {
-        int[] check = new int[cards.length];
-        ArrayList<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < cards.length ; i ++){
-            if(check[i] != 0) continue;
-            int cnt = 1;
-            check[i]++;
-            int j = cards[i] -1;
-            while(i != j){
-                cnt++;
-                check[j]++;
-                j = cards[j] - 1;
+        this.cards = cards;
+        
+        list = new ArrayList<>();
+        
+        n = cards.length;
+        
+        visited = new boolean[n];
+        
+        for (int i = 0 ; i < n ; i ++){
+            if (!visited[i]){
+                int size = calc(i);
+                list.add(size);
             }
-            list.add(cnt);
         }
         
-        list.sort((a,b) -> b- a);
-        return list.size() == 1 ? 0 : list.get(0) * list.get(1);
+        list.sort(Collections.reverseOrder());
+        
+        int answer = 0;
+        
+        if (list.size() != 1) answer = list.get(0) * list.get(1);
+        
+        return answer;
+    }
+    
+    public int calc(int idx){
+        int cur = idx;
+        
+        int count = 0;
+        int next = 0;
+        
+        while (true){
+            visited[cur] = true;
+            count++;
+            
+            next = cards[cur] - 1;
+            
+            if (visited[next]) break;
+            
+            cur = next;
+        }
+        
+        return count;
     }
 }
