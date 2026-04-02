5b72a493d2a1eb16984324bc811afc908f13b200
park minkyu <149496018+playous@users.noreply.github.com>
2026-04-02 21:17:08 +0900

    [level 3] Title: 등굣길, Time: 3.05 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 53.6 MB, 시간: 1.46 ms
+메모리: 53.9 MB, 시간: 3.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 18:28:28
+2026년 04월 02일 21:17:06
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,21 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] map = new int[m+1][n+1];
-        boolean[][] water = new boolean[m+1][n+1];
-    
-        for (int[] arr : puddles){
-            water[arr[0]][arr[1]] = true;
-        }
-        map[0][1] = 1;
         
-        for (int i = 1;  i <= m ; i++){
-            for (int j = 1; j <= n ; j++){
-                if(water[i][j] == false){
-                   map[i][j] = (map[i-1][j] + map[i][j-1]) % 1000000007; 
-                }
+        long[][] dp = new long[m + 1][n + 1];
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int i = 0 ; i < puddles.length; i ++){
+            set.add(puddles[i][0] * 101 + puddles[i][1]);
+        }
+        
+        dp[0][1] = 1;
+        
+        for (int i = 1 ; i <= m ; i ++){
+            for (int j = 1 ; j <= n ; j++){
+                int key = i * 101 + j;
+                if (set.contains(key)) continue;
+                
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
             }
         }
-       
-        return map[m][n];
+        
+        
+        return (int)dp[m][n];
     }
 }
