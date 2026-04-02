3f18d972b3b258fcb54fd12436317805a6c50a69
park minkyu <149496018+playous@users.noreply.github.com>
2026-04-02 21:17:57 +0900

    [level 2] Title: 바이러스 파이프, Time: 15.05 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
@@ -2,78 +2,78 @@ import java.util.*;
 
 class Solution {
     
+    List<Integer>[][] list;
     int n;
-    int answer;
-    List<Integer>[][] graph;
-    
-    public class Node{
-        int a;
-        int b;
-        Node(int a, int b){
-            this.a = a;
-            this.b = b;
-        }
-    }
+    int answer = 0;
     
     public int solution(int n, int infection, int[][] edges, int k) {
-        this.answer = 1;
-        this.n = n;
-        boolean[] visited = new boolean[n + 1];
-
-        graph = new List[4][n + 1];
         
-        for (int i = 1 ; i <= 3; i++){
-            for (int j = 1; j<= n ; j++){
-                graph[i][j] = new ArrayList<>();
+        this.n = n;
+        list = new List[n + 1][4];
+        
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= 3; j ++){
+                list[i][j] = new ArrayList<>();
             }
         }
         
-        for (int i = 0; i < edges.length; i ++){
+        for (int i = 0 ; i < edges.length ; i ++){
             int a = edges[i][0];
             int b = edges[i][1];
-            int t = edges[i][2];
-  
-            graph[t][a].add(b);
-            graph[t][b].add(a);
+            int type = edges[i][2];
             
+            list[a][type].add(b);
+            list[b][type].add(a);
+
         }
         
-        visited[infection] = true;
+        List<Integer> infec = new ArrayList<>();
+        infec.add(infection);
         
-        calc(k, 0, visited, 1);
+        calc(k, -1, infec);
         
         return answer;
     }
     
-    public void calc(int k, int prev, boolean[] visited1, int count){
-        if (k == 0) {
-            answer = Math.max(answer, count);
+    public void calc(int cnt, int before, List<Integer> infec){
+        
+        if (cnt == 0){
+            answer = Math.max(infec.size(), answer);
             return;
         }
-        for (int type = 1; type <= 3 ; type ++){
-            if (type == prev) continue;
-            boolean[] visited = visited1.clone();
-            int cnt = count;
+        
+        for (int i = 1; i <= 3; i ++){
+            if (i == before) continue;
             
             Queue<Integer> q = new ArrayDeque<>();
             
-            for (int i = 1 ; i <= n ; i ++){
-                if(visited[i]) q.add(i);
+            boolean[] visited = new boolean[n + 1];
+            List<Integer> plus = new ArrayList<>();
+            
+            for (int inf : infec){
+                q.add(inf);
+                visited[inf] = true;
             }
             
-            while (!q.isEmpty()){
+            
+            while(!q.isEmpty()){
                 int cur = q.poll();
-                
-                for (int next : graph[type][cur]){
+                for (int next : list[cur][i]){
                     if (!visited[next]){
                         visited[next] = true;
+                        plus.add(next);
+                        infec.add(next);
                         q.add(next);
-                        cnt++;
                     }
                 }
             }
             
-            calc(k - 1, type, visited, cnt);
+            calc(cnt - 1, i, infec);
+            
+            for (int remove : plus){
+                infec.remove((Object) remove);
+            }
+            
         }
     }
 }
