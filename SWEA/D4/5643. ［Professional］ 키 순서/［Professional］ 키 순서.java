5164db4f5a2bf07b9aa3f521474407bfc7e20ac8
park minkyu <149496018+playous@users.noreply.github.com>
2026-04-14 11:56:30 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,298 ms, Memory: 101,332 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,65 +1,86 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
-    static int n, m;
-    static List<Integer>[] list;
-    static HashSet<Integer>[] set;
-    static boolean[] visited;
-
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int T = Integer.parseInt(br.readLine());
-
-        for (int t = 1; t <= T; t++) {
-            n = Integer.parseInt(br.readLine());
-            m = Integer.parseInt(br.readLine());
-
-            list = new List[n + 1];
-            set = new HashSet[n + 1];
-
-            for (int i = 1; i <= n; i++) {
-                list[i] = new ArrayList<>();
-                set[i] = new HashSet<>();
-            }
-
-            for (int i = 0; i < m; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                list[a].add(b);
-            }
-            for (int i = 1; i <= n; i++) {
-                visited = new boolean[n + 1];
-                dfs(i, i);
-            }
-
-            int answer = 0;
-            for (int i = 1; i <= n; i++) {
-                if (set[i].size() == n - 1){
-                    answer++;
-                }
-            }
-
-            sb.append("#").append(t).append(" ").append(answer).append("\n");
-        }
-
-        System.out.print(sb.toString());
-    }
-
-    static void dfs(int idx, int start){
-        visited[idx] = true;
-        for (int next : list[idx]) {
-            if (!visited[next]) {
-                visited[next] = true;
-                set[next].add(start);
-                set[start].add(next);
-                dfs(next, start);
-            }
-        }
-    }
-
+	
+	public static boolean[] visited;
+	public static int n, m;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1 ; t <= T ; t++) {
+			n = Integer.parseInt(br.readLine());
+			
+			m = Integer.parseInt(br.readLine());
+			
+			// 정방향
+			List<Integer>[] list1 = new List[n + 1];
+			
+			// 역방향
+			List<Integer>[] list2 = new List[n + 1];
+		
+			for (int i = 1 ; i <= n ; i ++) {
+				list1[i] = new ArrayList<>();
+				list2[i] = new ArrayList<>();
+			}
+			
+			for (int i = 0 ; i < m ; i ++) {
+				st = new StringTokenizer(br.readLine());
+				
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				list1[a].add(b);
+				list2[b].add(a);
+			}
+			
+			int answer = 0;
+			
+			for (int i = 1; i <= n ; i++) {
+				
+				// 나보다 큰 사람 + 작은사람 
+				visited = new boolean[n + 1];
+				
+				bfs(list1, i);
+				bfs(list2, i);
+				
+				int cnt = 0;
+				
+				for (int j = 1 ; j <= n ; j ++) {
+					if (i == j) continue;
+					if (visited[j]) cnt ++;
+				}
+				
+				if (cnt == n - 1) answer++;
+				
+			}
+			
+			
+			
+			sb.append("#").append(t).append(" ").append(answer).append("\n");
+		}
+		
+		System.out.print(sb.toString());
+	}
+	
+	public static void bfs(List<Integer>[] list, int idx) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(idx);
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			
+			for (int next : list[cur]) {
+				if (!visited[next]) {
+					q.add(next);
+					visited[next] = true;
+				}
+			}	
+		}
+	}
 }
