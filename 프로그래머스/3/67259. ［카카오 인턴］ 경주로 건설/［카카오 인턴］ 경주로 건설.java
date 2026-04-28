c9b5fee9fa2833a62d8870f0eb979d40d217fc43
park minkyu <149496018+playous@users.noreply.github.com>
2026-04-28 21:57:35 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 3.36 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,73 +1,74 @@
 import java.util.*;
 
-class Solution {
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-    static int[][] visited;
-    static int n;
-    static int answer;
+class Solution {    
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, 1, -1};
     
-    static class node{
+    class Node{
         int r;
         int c;
-        int dic; // 방향 (0~3) (어디서 왔는지)
         int cost;
-        node(int r, int c, int dic, int cost){
+        int flag;
+        Node(int r, int c, int cost, int flag){
             this.r = r;
             this.c = c;
-            this.dic = dic;
             this.cost = cost;
+            this.flag = flag;
         }
     }
     
+    
     public int solution(int[][] board) {
-        answer = 0;
-        n = board.length;
-        visited = new int[n][n];
+        int n = board.length;
+        
+        int[][] visited = new int[n][n];
         
         for (int i = 0 ; i < n ; i ++){
-            for (int j = 0 ; j < n ; j++){
+            for (int j = 0; j < n ; j ++){
                 visited[i][j] = Integer.MAX_VALUE;
             }
         }
+        PriorityQueue<Node> q = new PriorityQueue<>((a, b) -> a.cost - b.cost);
         
-        bfs(board);
+        if (board[0][1] == 0) {
+            q.add(new Node(0, 1, 100, 2));
+            visited[0][1] = 100;
+        }
+        if (board[1][0] == 0) {
+            q.add(new Node(1, 0, 100, 1));
+            visited[1][0] = 100;
+        }
         
-        return answer;
-    }
-    
-    private static void bfs(int[][] board){
-        PriorityQueue<node> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
+        /*
+            위 = 0
+            아래 = 1
+            오른쪽 = 2
+            왼쪽 = 3
+        */
         
-        pq.add(new node(0,0,4,0)); // 시작점 4 -> 무조건 직진
-        visited[0][0] = 0;
-        
-        while(!pq.isEmpty()){
-            node cur = pq.poll();
-            int r = cur.r;
-            int c = cur.c;
-            int prevDic = cur.dic;
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            int cr = cur.r;
+            int cc = cur.c;
             int cost = cur.cost;
+            int flag = cur.flag;
             
-            if (r == n - 1 && c == n - 1){
-                answer = cost;
-                return;
-            }
+             if (cr == n-1 && cc == n-1) return cost;
             
             for (int i = 0 ; i < 4 ; i ++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                int dic = i;
+                int nr = cr + dr[i];
+                int nc = cc + dc[i];
                 int nCost = cost + 100;
-                if (prevDic != dic && prevDic != 4) nCost += 500;
-                
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n){
-                    if (nCost - 399 <= visited[nr][nc] && board[nr][nc] == 0){ 
-                        pq.add(new node(nr,nc,dic,nCost));
+                if (i != flag) nCost += 500;
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n){
+                    if (board[nr][nc] == 0 && nCost - 399 <= visited[nr][nc]){
                         visited[nr][nc] = nCost;
+                        q.add(new Node(nr, nc, nCost, i));
                     }
                 }
             }
         }
+        
+        return visited[n-1][n-1];
     }
 }
