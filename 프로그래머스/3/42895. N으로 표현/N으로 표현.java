210b7d78b9d22603b9954d4d9033fecd6d9ddd17
park minkyu <149496018+playous@users.noreply.github.com>
2026-04-28 21:32:01 +0900

    [level 3] Title: N으로 표현, Time: 27.16 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,37 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        Set<Integer>[] set = new HashSet[9];
+        int answer = -1;
+        
+        HashSet<Integer>[] set = new HashSet[9];
+        
+        int num = N;
         
         for (int i = 1 ; i <= 8 ; i ++){
             set[i] = new HashSet<>();
+            set[i].add(num);
+            num *= 10;
+            num += N;
         }
+                
+        if (N == number) return 1;
         
-        int n = N;
-        for (int i = 1; i <= 8 ; i ++){
-            set[i].add(n);
-            n *= 10;
-            n += N;
-        }
-        
-        for (int i = 1; i<= 8; i ++){
-            for (int j = 1; j < i ; j++){
-                int next = i - j;
-                if (next > 8 || next == 0) continue;
-                for (int a : set[j]){
-                    for (int b : set[i-j]){
-                        set[i].add(a + b);
-                        set[i].add(a - b);
-                        set[i].add(a * b);
-                        if (b != 0) set[i].add(a / b);
+        for (int i = 2; i <= 8 ; i ++){
+            
+            for (int j = 1 ; j < i ; j++){
+                int left = j;
+                int right = i - j;
+                
+                for (int num1 : set[left]){
+                    for (int num2 : set[right]){
+                        set[i].add(num1 + num2);
+                        set[i].add(num1 - num2);
+                        set[i].add(num1 * num2);
+                        if (num2 != 0) set[i].add(num1/num2);
                     }
-                }
+                }    
+            
             }
             if (set[i].contains(number)){
-                return i;
-             }
+                answer = i;
+                break;
+            }
         }
-    
-        return -1;
+      
+        return answer;
     }
 }
