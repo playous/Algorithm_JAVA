83cc89487af31cd4c97b5698762aa1a85b356987
park minkyu <149496018+playous@users.noreply.github.com>
2026-04-28 19:40:33 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,28 +2,44 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        LinkedList<Integer> list = new LinkedList<>();
-        Deque<Integer> stack = new ArrayDeque<>();
+        int n = progresses.length;
         
-        for (int i = 0 ; i < speeds.length; i++){
-            stack.addLast((int) Math.ceil((100.0 - progresses[i]) / speeds[i]));
+        List<Integer> list = new ArrayList<>();
+        
+        int[] time = new int[n];
+
+        for (int i = 0; i < n ; i ++){
+            int need = 100 - progresses[i];
+            
+            time[i] = need/speeds[i];
+            
+            if (need % speeds[i] != 0) time[i]++;
         }
         
-        while(!stack.isEmpty()){
-            int cnt = 1;
-            int n = stack.removeFirst();
-            while(!stack.isEmpty() && n >= stack.peekFirst()){
-                cnt++;
-                stack.removeFirst();
+        boolean[] visited = new boolean[n];
+        
+        for (int i = 0 ; i < n ; i ++){
+            if (visited[i]) continue;
+            
+            int cur = time[i];
+            visited[i] = true;
+            int count = 1;
+            
+            for (int j = i + 1 ; j < n ; j ++){
+                if (time[j] <= cur){
+                    visited[j] = true;
+                    count++;
+                }
+                else break;
             }
-            list.add(cnt);
+            
+            list.add(count);
         }
-        int[] answer = new int[list.size()];
         
-        int index = 0;
+        int[] answer = new int[list.size()]; 
         
-        for (int i : list){
-            answer[index++] = i;
+        for (int i = 0 ; i < answer.length; i ++){
+            answer[i] = list.get(i);
         }
         return answer;
     }
