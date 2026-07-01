a3ee00a192ed266fabe1fe9373f724c3cddd934b
park minkyu <149496018+playous@users.noreply.github.com>
2026-07-01 14:36:57 +0900

    [level 2] Title: 피보나치 수, Time: 2.29 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[100001];
+        
+        dp[1] = 1;
+        
+        for (int i = 2 ; i <= 100000; i ++){
+            dp[i] = (dp[i-1] + dp[i - 2]) % 1234567;
+        }
+        
+        int answer = dp[n];
+        return answer;
+    }
+}
