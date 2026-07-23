e6346f5ceb5967c9c376e11b8659d39f94e73770
park minkyu <149496018+playous@users.noreply.github.com>
2026-07-23 21:59:11 +0900

    [level 3] Title: 가장 먼 노드, Time: 17.36 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,24 +1,25 @@
 import java.util.*;
 
 class Solution {
-    static class Node{
-        int name;
+    class Node{
+        int id;
         int level;
-        Node (int name, int level){
-            this.name = name;
+        Node(int id, int level){
+            this.id = id;
             this.level = level;
         }
     }
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        List<Integer>[] graph = new List[n+1];
-        boolean[] visited = new boolean[n+1];
+        List<Integer>[] graph = new List[n + 1];
         
-        for (int i = 1 ; i<= n ; i++){
+        boolean[] visited = new boolean[n + 1];
+        int[] level = new int[20000];
+        
+        for (int i = 1 ; i <= n ; i ++){
             graph[i] = new ArrayList<>();
         }
         
-        for (int i = 0 ; i < edge.length ; i++){
+        for (int i = 0; i < edge.length ; i ++){
             int a = edge[i][0];
             int b = edge[i][1];
             graph[a].add(b);
@@ -26,30 +27,27 @@ class Solution {
         }
         
         Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(1,0));
-        visited[1] = true;
+        
         int max = 0;
         
-        while(!q.isEmpty()){
+        q.add(new Node(1, 0));
+        visited[1] = true;
+        
+        while (!q.isEmpty()){
             Node cur = q.poll();
-            int c_name = cur.name;
-            int n_level = cur.level + 1;
-            if (n_level == max){
-                answer ++;
-            }
-            if (n_level > max) {
-                max = n_level;
-                answer = 1;
-            }
+            level[cur.level]++;
             
-            for (int next : graph[c_name]){
-                if(!visited[next]){
-                    q.add(new Node(next, n_level));
+            max = Math.max(max, cur.level);
+            
+            for (int next : graph[cur.id]){
+                if (!visited[next]){
                     visited[next] = true;
+                    q.add(new Node(next, cur.level + 1));
                 }
             }
         }
         
-        return answer;
+        return level[max];
     }
+    
 }
