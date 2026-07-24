7a8f43b8307b44ae88a861b102e6862a00259f9b
park minkyu <149496018+playous@users.noreply.github.com>
2026-07-24 09:13:15 +0900

    [level 3] Title: 순위, Time: 6.08 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,61 +1,59 @@
 import java.util.*;
 
 class Solution {
+    int n;
     public int solution(int n, int[][] results) {
         int answer = 0;
-        int len = results.length;
         
-        List<Integer>[] win_list = new List[n+1];
-        List<Integer>[] def_list = new List[n+1];
+        this.n = n;
+        
+        List<Integer>[] graphIn = new List[n + 1];
+        List<Integer>[] graphOut = new List[n + 1]; 
         
         for (int i = 1; i <= n ; i ++){
-            win_list[i] = new ArrayList<>();
-            def_list[i] = new ArrayList<>();
+            graphIn[i] = new ArrayList<>();
+            graphOut[i] = new ArrayList<>();
         }
         
-        for (int i = 0 ; i < len ; i++){
-            int win = results[i][0];
-            int def = results[i][1];
-            win_list[win].add(def);
-            def_list[def].add(win);
+        for (int[] arr : results){
+            int a = arr[0];
+            int b = arr[1];
+            
+            graphIn[a].add(b);
+            graphOut[b].add(a);
         }
         
-        for (int i = 1; i <= n ; i++){
-            boolean[] visited = new boolean[n+1];
-            visited[i] = true;
-            
-            Queue<Integer> win_q = new ArrayDeque<>();
-            Queue<Integer> def_q = new ArrayDeque<>();
-            win_q.add(i);
-            def_q.add(i);
-                        
-            int count = 1;
-            
-            while (!win_q.isEmpty()){
-                int cur = win_q.poll();
-                for (int next : win_list[cur]){
-                    if (!visited[next]){
-                        visited[next] = true;
-                        count++;
-                        win_q.add(next);
-                    }
-                }
-            }
-              
-            while (!def_q.isEmpty()){
-                int cur = def_q.poll();
-                for (int next : def_list[cur]){
-                    if (!visited[next]){
-                        visited[next] = true;
-                        count++;
-                        def_q.add(next);
-                    }
-                }
-            }
-            
-            if (count == n) answer ++;
-            
+        for (int i = 1 ; i <= n; i ++){
+            int in = bfs(i, graphIn);
+            int out = bfs(i, graphOut);
+            if (in + out == n - 1) answer++;
         }
+        
         return answer;
+    }
+    
+    public int bfs (int id, List<Integer>[] graph){
+        int cnt = 0;
+        
+        boolean[] visited = new boolean[n + 1];
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(id);
+        visited[id] = true;
+        
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            
+            for (int next : graph[cur]){
+                if (!visited[next]){
+                    visited[next] = true;
+                    q.add(next);
+                    cnt++;
+                }
+            }
+        }
+        
+        return cnt;
+        
     }
 }
