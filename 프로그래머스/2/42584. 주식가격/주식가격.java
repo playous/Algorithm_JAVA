6e6cbd2ff3c600493d98d453053a34c3664f473d
park minkyu <149496018+playous@users.noreply.github.com>
2026-07-25 15:29:24 +0900

    [level 2] Title: 주식가격, Time: 14.15 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,20 +4,22 @@ class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        Deque<Integer> stack = new ArrayDeque<>(); 
         
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int top = stack.pop();
-                answer[top] = i - top; 
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < n ; i ++){
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                int id = stack.pop();
+                answer[id] = i - id;
             }
-            stack.push(i); 
+            stack.push(i);
         }
         
-        while (!stack.isEmpty()) {
-            int top = stack.pop();
-            answer[top] = n - 1 - top; 
+        while(!stack.isEmpty()){
+            int id = stack.pop();
+            answer[id] = n - id - 1; 
         }
+        
         
         return answer;
     }
