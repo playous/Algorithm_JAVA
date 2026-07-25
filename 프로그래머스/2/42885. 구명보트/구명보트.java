7df090c70c1762027ef4e5b0d4fcb69b66329dd8
park minkyu <149496018+playous@users.noreply.github.com>
2026-07-25 16:20:54 +0900

    [level 2] Title: 구명보트, Time: 9.08 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,29 +4,21 @@ import java.io.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        
         Arrays.sort(people);
         
         int left = 0;
         int right = people.length - 1;
         
-        while (left <= right){
-            
-            int min = people[left];
-            int max = people[right];
-            
-            int sum = min + max;
-            
-            if (sum <= limit){
+        while(left < right){
+            if (people[left] + people[right] <= limit){
                 left++;
                 right--;
             }
-            else {
-                right--;
-            }
+            else right--;
             answer++;
-            
         }
+        if (left == right) answer++;
+        
         return answer;
     }
 }
